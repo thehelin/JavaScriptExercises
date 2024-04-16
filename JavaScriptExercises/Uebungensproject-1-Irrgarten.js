08ee02c7eb271d1976903e67c7174fb7893f1ace
Thehelin <demirhelin00@gmail.com>
2024-04-16 13:33:35 +0200

    Fixed the problems

--- JavaScriptExercises/Uebungensproject-1-Irrgarten.js
+++ JavaScriptExercises/Uebungensproject-1-Irrgarten.js
@@ -35,36 +35,47 @@ const movePlayer = async () => {
     const direction = await prompt('Move (u: up, d: down, l: left, r: right, x: exit): ');
     switch (direction) {
         case 'u':
-            if (labyrinth[player.y - 1][player.x] !== '#') player.y--;
+            if (player.y > 0 && labyrinth [player.y - 1][player.x] !== '[ # ]') {
+                player.y--;
+            }
             break;
         case 'd':
-            if (labyrinth[player.y + 1][player.x] !== '#') player.y++;
+            if (player.y < labyrinth.length - 1 && labyrinth [player.y + 1][player.x] !== '[ # ]') {
+                player.y++;
+            }
             break;
         case 'l':
-            if (maze[player.y][player.x - 1] !== '#') player.x--;
+            if (player.x > 0 && labyrinth[player.y][player.x - 1] !== '[ # ]') {
+                player.x--;
+            }
             break;
         case 'r':
-            if (labyrinth[player.y][player.x + 1] !== '#') player.x++;
+            if (player.x < labyrinth[player.y].length - 1 && labyrinth[player.y][player.x + 1] !== '[ # ]') {
+                player.x++;
+            }
             break;
         case 'x':
             console.log('Game Exited');
             rl.close();
             process.exit(0);
             return;
+
     }
     if (player.x === goal.x && player.y === goal.y) {
         console.log('Congratulations you reached the goal');
         rl.close();
         process.exit(0);
     } else {
-        movePlayer(); //oyun bitene kadar tekrarla
+        await movePlayer(); //oyun bitene kadar tekrarla
     }
 };
 
 async function execute() {
     const name = await prompt("Please enter your name: ");
     console.log(`Hello, ${name} Welcome to the Labyrinth`)
-    movePlayer();
+    await movePlayer();
 }
 
-execute().catch((err) => {console.error(err);}).finally(() => rl.close());
+execute().catch((err) => {
+    console.error(err);
+}).finally(() => rl.close());
