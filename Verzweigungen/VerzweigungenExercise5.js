b18e03eac0843a5c833d6ea22139884e6d8090f3
Thehelin <demirhelin00@gmail.com>
2024-04-17 09:13:41 +0200

    Fixed the problem of Verzweigungen Exercise 5

--- Verzweigungen/VerzweigungenExercise5.js
+++ Verzweigungen/VerzweigungenExercise5.js
@@ -5,23 +5,29 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function vowelOrConsonant() {
-    const letter = await prompt("Please enter a single letter: ")
-    console.log('Letter', letter);
+    const letter = await prompt("Please enter a single letter: ");
+    const normalizeLetter = letter.toLowerCase();
+    if (normalizeLetter.length !== 1 || !/[a-z]/i.test(normalizeLetter)) {
+        console.log('Please enter a valid single letter');
+    } else {
 
-    switch (letter) {
-        case'a':
-        case'e':
-        case'u':
-        case'o':
-        case'i':
-            console.log('It is a vowel')
-            break;
-        default:
-            console.log('It is a consonant');
-            break
+
+        switch (letter) {
+            case'a':
+            case'e':
+            case'u':
+            case'o':
+            case'i':
+                console.log('It is a vowel')
+                break;
+            default:
+                console.log('It is a consonant');
+        }
+
     }
-
 }
+
+
 
 vowelOrConsonant().catch((err) => {
     console.error(err);
